86a9215b5955abb45aa87b8af4186ccdd9f6006c
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2021-01-23 19:23:55 +0600

    maked timer made a timer until the new year

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const daysEl  = document.querySelector("#days");
+      hoursEl = document.querySelector("#hours");
+      minsEl = document.querySelector("#mins");
+      secondEl = document.querySelector("#second");
+
+
+const newYears = '1 Jan 2022';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondEl.innerHTML = formatTime(seconds);
+
+    function formatTime(time){
+        return time < 10 ? (`0${time}`) : time;
+    }
+
+    console.log(days, hours, minutes, seconds);
+}
+
+countdown();
+
+setInterval(countdown, 1000);
